c209fc540c3978e431bca3e3108c49bcfdeafd55
oldfour8888 <125353726+oldfour8888@users.noreply.github.com>
2025-03-30 18:11:34 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -6,16 +6,16 @@ $token = $_REQUEST['token'] ?? null;
 $message = $_REQUEST['message'] ?? '';
 
 $bot = new Bot();
-error_log("Token: " . json_encode($token));
-error_log("Message: " . json_encode($message));
+console.log("Token: " . json_encode($token));
+console.log("Message: " . json_encode($message));
 if (is_null($token)) {
     echo json_encode(['code' => 422, 'message' => 'token 不能为空']);
 } else {
     // 发送消息
     $chat_id = $bot->decryption($token);
-    error_log("Chat ID: " . json_encode($chat_id));
+    console.log("Chat ID: " . json_encode($chat_id));
     $ret = $bot->sendMessage(['text' => $message, 'chat_id' => $chat_id]);
-    error_log("Response: " . json_encode($ret));
+    console.log("Response: " . json_encode($ret));
     if ($ret['ok']) {
         echo json_encode(['code' => 200, 'message' => 'success']);
     } else {
